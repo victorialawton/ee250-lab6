e8403e01630921c55c17c06eb71ef6031a2c1f0b
Sriya Kuruppath <kuruppat@usc.edu>
2024-02-27 12:06:13 -0800

    Create grovepi_sensors.py

---
+++ grovepi_sensors.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append('~/Dexter/GrovePi/Software/Python')
+import time
+import grovepi
+from grove_rgb_lcd import *
+
+# Grove Ultrasonic Ranger connectd to digital port 2
+ultrasonic_ranger = 2
+# potentiometer connected to analog port A0 as input
+potentiometer = 0
+grovepi.pinMode(potentiometer,"INPUT")
+
+# clear lcd screen  before starting main loop
+setText("")
+
+while True:
+  try:
+    # TODO:read distance value from Ultrasonic Ranger and print distance on LCD
+  
+
+    # TODO: read threshold from potentiometer
+
+    
+    # TODO: format LCD text according to threshhold
+  
+    
+  except IOError:
+    print("Error")
